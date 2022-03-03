5b059489a727c735011f29a1074a014c509978bd
Timur Bakibayev <timurbakibayev@gmail.com>
2022-03-03 09:00:37 +0100

    static files, all libraries are now local

--- almauadv/urls.py
+++ almauadv/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin  # type: ignore
 from django.http import HttpRequest, HttpResponse  # type: ignore
 from django.urls import path  # type: ignore
+from django.conf.urls.static import static
+from almauadv import settings
 from app.views_cars import (
     index,
     delete,
@@ -19,4 +21,4 @@ urlpatterns = [
     path('cars/<int:car_id>/trips', car_trips),
     path('delete_trip/<int:trip_id>', car_trips_delete),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
